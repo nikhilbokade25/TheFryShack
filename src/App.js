f8122c5b045c140d6dacf398da88a1162d1b586a
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-04-21 12:54:47 -0400

    Implement Products Component with responsive design

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero';
 import About from './Components/About';
+import Products from './Components/Products';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <About/>
+      <Products/>
     </div>
   );
 }
---
+++ src/Components/Products.jsx
@@ -0,0 +1,59 @@
+import './Products.css';
+
+import pizzaImg from "../Assets/pizzaImg.jpg";
+import burgerImg from "../Assets/burgerImg.jpg";
+import nachosImg from "../Assets/nachosImg.jpg";
+
+export default function Products() {
+    return (
+        <div className="product_container">
+            <div className="heading_wrapper">
+                <h1 className="main_heading">
+                    <span>CRUNCHY </span>
+                    <i>CRUNCHY </i>
+                    <span>CRUNCHY </span>
+                    <i>CRUNCHY </i>
+                    <span>CRUNCHY </span>
+                    <i>CRUNCHY </i>
+                    <span>CRUNCHY</span>
+                </h1>
+            </div>
+
+            <div className="product_content">
+                
+                <div className='product_box'>
+                    <div className='product_image'>
+                        <img src={pizzaImg} alt='pizza'/>
+                    </div>
+
+                    <div className='product_text'>
+                        <h1>PIZZA</h1>
+                        <p>Cheesy, crispy, and loaded with toppings.</p>
+                    </div>
+                </div>
+
+                <div className='product_box'>
+                    <div className='product_text second_box_text'>
+                        <h1>BURGER</h1>
+                        <p>Juicy beef burger with fresh veggies.</p>
+                    </div>
+
+                    <div className='product_image second_box_image'>
+                        <img src={burgerImg} alt='burger' />
+                    </div>
+                </div>
+
+                <div className='product_box'>
+                    <div className='product_image'>
+                        <img src={nachosImg} alt='nachos' />
+                    </div>
+                    <div className='product_text '>
+                        <h1>NACHOS</h1>
+                        <p>Crunchy nachos with gooey cheese and dips.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
